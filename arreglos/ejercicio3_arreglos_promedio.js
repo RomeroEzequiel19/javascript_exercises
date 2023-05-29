c405cdbcda34a633200c09fe375092b5724d25a2
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-05-29 09:27:59 -0300

    Arreglando detalles

--- arreglos/ejercicio3_arreglos_promedio.js
+++ arreglos/ejercicio3_arreglos_promedio.js
@@ -2,19 +2,22 @@
 
 //Declaro las variables y le asigno un arreglo
 let notasEstudiantes = [9, 10, 6, 8, 4, 10, 8, 9];
-let pesos = [0.5, 4.5, 0.9, 0.1, 1.34, 1.02, 1.2, 0.39];
+let pesosEstudiantes = [0,7, 0.5, 0,60, 0.34, 0.65, 0.67, 0.39];
 
+//Creo la funcion obtenerPromedioPonderado que retorna el promedio ponderado
 function obtenerPromedioPonderado(notas, pesos) {
   let sumaNotas = 0;
   let sumaPesos = 0;
 
   for (let i = 0; i < notas.length; i++) {
-    sumaNotas += notas[i] * pesos[i];
-    sumaPesos += pesos[i];
+    //Multiplico cada dato por su ponderacion y sumo sus valores
+    sumaNotas = sumaNotas + notas[i] * pesos[i];
+    sumaPesos = sumaPesos + pesos[i];
   }
-
-  const PromedioPonderado = sumaNotas / sumaPesos;
-  return PromedioPonderado;
+  //Dividimos la suma de las notas con su ponderación por la suma de los pesos
+  let PromedioPonderado = sumaNotas / sumaPesos;
+  return `El promedio ponderado es: ${PromedioPonderado}`;
 }
 
-console.log(obtenerPromedioPonderado(notasEstudiantes, pesos));
+//Le paso los parámetros de las notas y los pesos y la función obtenerPromedioPonderado, y muestro por consola su retorno
+console.log(obtenerPromedioPonderado(notasEstudiantes, pesosEstudiantes));
